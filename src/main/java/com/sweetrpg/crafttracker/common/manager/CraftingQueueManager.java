babebe2bff388d1f5eb6f00d418dea8722889018
Paul Schifferer <paul@schifferers.net>
2024-11-22 20:39:00 -0800

    Check if player has intermediate item

--- src/main/java/com/sweetrpg/crafttracker/common/manager/CraftingQueueManager.java
+++ src/main/java/com/sweetrpg/crafttracker/common/manager/CraftingQueueManager.java
@@ -4,6 +4,7 @@ import com.sweetrpg.crafttracker.CraftTracker;
 import com.sweetrpg.crafttracker.common.model.CraftingQueueProduct;
 import com.sweetrpg.crafttracker.common.storage.CraftingQueueStorage;
 import com.sweetrpg.crafttracker.common.util.RecipeUtil;
+import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtIo;
 import net.minecraft.resources.ResourceLocation;
@@ -233,9 +234,6 @@ public class CraftingQueueManager {
 
         if(RecipeUtil.areIngredientsSame(ingredients)) {
             CraftTracker.LOGGER.debug("ingredients are the same: {}", ingredients);
-//            if(ingredients.get(0) instanceof Ingredient ingredient) {
-//                var item = ingredient.getItems()[0];
-//                var id = item.getItem().getRegistryName();
             var id = recipe.getId();
             CraftTracker.LOGGER.debug("id: {}", id);
             if(this.intermediateProducts.containsKey(id)) {
@@ -247,7 +245,6 @@ public class CraftingQueueManager {
 
                 return;
             }
-//            }
         }
 
         ingredients.stream()
@@ -275,14 +272,34 @@ public class CraftingQueueManager {
                                     }
                                     else {
                                         CraftTracker.LOGGER.debug("subRecipes has {} items; intermediate", subRecipes.size());
-                                        // intermediate
-                                        this.intermediateProducts.compute(id, (itemId, quantity) -> {
-                                            if(quantity == null) {
-                                                return item.getCount();
-                                            }
 
-                                            return quantity + item.getCount();
-                                        });
+                                        // check if player already has the item
+                                        var inventory = Minecraft.getInstance().player.getInventory();
+                                        if(inventory.contains(item)) {
+                                            inventory.items.stream()
+                                                    .filter((inv) -> inv.getItem().getRegistryName().equals(id))
+                                                    .map((inv) -> inv.getCount())
+                                                    .findFirst()
+                                                    .ifPresent((count) -> {
+                                                        this.intermediateProducts.compute(id, (itemId, quantity) -> {
+                                                            Integer finalCount = (quantity == null ? 0 : quantity) + item.getCount() - count;
+                                                            if(finalCount < 1) {
+                                                                return null;
+                                                            }
+
+                                                            return finalCount;
+                                                        });
+                                                    });
+                                        }
+                                        else {
+                                            this.intermediateProducts.compute(id, (itemId, quantity) -> {
+                                                if(quantity == null) {
+                                                    return item.getCount();
+                                                }
+
+                                                return quantity + item.getCount();
+                                            });
+                                        }
 
                                         this.computeRecipe(subRecipes.get(0), recipeQuantity);
                                     }
