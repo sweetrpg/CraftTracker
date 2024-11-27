2233c04db1fd688b1e6f60e2fa71c98468cd379f
Paul Schifferer <paul@schifferers.net>
2024-11-26 21:24:44 -0800

    Cleanup

--- src/main/java/com/sweetrpg/crafttracker/common/util/RecipeUtil.java
+++ src/main/java/com/sweetrpg/crafttracker/common/util/RecipeUtil.java
@@ -18,50 +18,9 @@ public class RecipeUtil {
         CraftTracker.LOGGER.debug("RecipeUtil#getRecipesFor: {}", itemId);
 
         var mgr = Minecraft.getInstance().level.getRecipeManager();
-//        var ids = mgr.getRecipeIds()
-//                .filter(rl -> rl.equals(itemId))
-//                .collect(Collectors.toUnmodifiableList());
         var recipes = mgr.getRecipes().stream()
                 .filter(r -> r.getResultItem().getItem().getRegistryName().equals(itemId))
-//                .map(rl -> mgr.byKey(rl).get())
                 .collect(Collectors.toUnmodifiableList());
-
-//        var rm = CTPlugin.jeiRuntime.getRecipeManager();
-//
-//        var categories = rm.createRecipeCategoryLookup().get()
-//                .collect(Collectors.toUnmodifiableList());
-//        CraftTracker.LOGGER.debug("RecipeUtil#getRecipesFor: categories {}", categories);
-//
-//        var item = ForgeRegistries.ITEMS.getValue(itemId);
-//
-//        var recipes = rm.createRecipeCategoryLookup().get()
-////                .peek(c -> CraftTracker.LOGGER.debug("category: {}", c))
-//                .map(c -> c.getRecipeType())
-////                .peek(t -> CraftTracker.LOGGER.debug("type: {}", t))
-//                .flatMap(t -> rm.createRecipeLookup(t).get())
-////                .peek(r -> CraftTracker.LOGGER.debug("recipe: {}", r))
-////                .filter(r -> !((r instanceof ShapelessRecipe) ||
-////                        (r instanceof ShapedRecipe)))
-//                .filter(r -> {
-//                    return r instanceof Recipe;
-//                })
-//                .map(r -> Recipe.class.cast(r))
-//////                .peek(r -> CraftTracker.LOGGER.debug("Recipe: {}", r.getId()))
-////                .filter(r -> {
-////                    CraftTracker.LOGGER.debug("RecipeUtil#getRecipesFor: {}", r);
-////                    try {
-////                        var method = r.getClass().getMethod("getId");
-////                        if(method != null) {
-////                            var id = method.invoke(r);
-////                            return id.equals(itemId);
-////                        }
-////                    }
-////                    catch (Exception e) {
-////                    }
-////                    return false;
-////                })
-//////                .peek(r -> CraftTracker.LOGGER.debug("{}: {}", itemId, r))
-//                .collect(Collectors.toUnmodifiableList());
 
         CraftTracker.LOGGER.debug("RecipeUtil#getRecipesFor: recipes {}", recipes);
         return recipes;
