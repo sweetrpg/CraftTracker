b6770eaa4a67e122f28b36365dab3dfdd4ab640b
Paul Schifferer <paul@schifferers.net>
2024-12-14 05:39:40 -0800

    Remove unused event handlers

--- src/main/java/com/sweetrpg/crafttracker/common/event/EventHandler.java
+++ src/main/java/com/sweetrpg/crafttracker/common/event/EventHandler.java
@@ -9,11 +9,9 @@ import com.sweetrpg.crafttracker.common.registry.ModAdvancements;
 import com.sweetrpg.crafttracker.common.util.AdvancementUtil;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.ItemCraftedEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.ItemPickupEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.ItemSmeltedEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
@@ -22,18 +20,6 @@ import static com.sweetrpg.crafttracker.common.network.packet.data.QueueCommandD
 
 @Mod.EventBusSubscriber(modid = Constants.MOD_ID)
 public class EventHandler {
-
-    @SubscribeEvent
-    public void onEntitySpawn(final EntityJoinWorldEvent event) {
-        CraftTracker.LOGGER.trace("EventHandler#onEntitySpawn: {}", event);
-
-    }
-
-    @SubscribeEvent
-    public void playerLoggedIn(final PlayerLoggedInEvent event) {
-        CraftTracker.LOGGER.debug("EventHandler#playerLoggedIn: {}", event);
-
-    }
 
     @SubscribeEvent
     public void onItemCrafted(final ItemCraftedEvent event) {
