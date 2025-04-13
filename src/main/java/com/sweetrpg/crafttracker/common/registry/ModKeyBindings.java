6ac23c3859fafa89b2f8f18dd3b1653fb826cbd3
Paul Schifferer <paul@schifferers.net>
2025-04-13 07:10:02 -0700

    fix: Change default keybinding for toggle shopping list to 'H'

--- src/main/java/com/sweetrpg/crafttracker/common/registry/ModKeyBindings.java
+++ src/main/java/com/sweetrpg/crafttracker/common/registry/ModKeyBindings.java
@@ -11,7 +11,7 @@ public class ModKeyBindings {
 
     public static final KeyBinding ADD_TO_QUEUE_MAPPING = new KeyBinding(Constants.TRANSLATION_KEY_BINDINGS_ADDTOQUEUE_TITLE, KeyConflictContext.GUI, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_Q, Constants.KEY_BINDINGS_CATEGORY_TITLE);
     public static final KeyBinding TOGGLE_CRAFT_QUEUE_MAPPING = new KeyBinding(Constants.TRANSLATION_KEY_BINDINGS_TOGGLE_CRAFT_QUEUE_TITLE, KeyConflictContext.GUI, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_L, Constants.KEY_BINDINGS_CATEGORY_TITLE);
-    public static final KeyBinding TOGGLE_SHOPPING_LIST_MAPPING = new KeyBinding(Constants.TRANSLATION_KEY_BINDINGS_TOGGLE_SHOPPING_LIST_TITLE, KeyConflictContext.GUI, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_S, Constants.KEY_BINDINGS_CATEGORY_TITLE);
+    public static final KeyBinding TOGGLE_SHOPPING_LIST_MAPPING = new KeyBinding(Constants.TRANSLATION_KEY_BINDINGS_TOGGLE_SHOPPING_LIST_TITLE, KeyConflictContext.GUI, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_H, Constants.KEY_BINDINGS_CATEGORY_TITLE);
     public static final KeyBinding OPEN_QUEUE_MANAGER_MAPPING = new KeyBinding(Constants.TRANSLATION_KEY_BINDINGS_OPEN_QMGR_TITLE, KeyConflictContext.GUI, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_M, Constants.KEY_BINDINGS_CATEGORY_TITLE);
     public static final KeyBinding POPULATE_SHOPPING_LIST_MAPPING = new KeyBinding(Constants.TRANSLATION_KEY_BINDINGS_POPULATE_SHOPPING_LIST_TITLE, KeyConflictContext.GUI, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_P, Constants.KEY_BINDINGS_CATEGORY_TITLE);
     public static final KeyBinding CLEAR_SHOPPING_LIST_MAPPING = new KeyBinding(Constants.TRANSLATION_KEY_BINDINGS_CLEAR_SHOPPING_LIST_TITLE, KeyConflictContext.GUI, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_K, Constants.KEY_BINDINGS_CATEGORY_TITLE);
